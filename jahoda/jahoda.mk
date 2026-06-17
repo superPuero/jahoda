050d229535af33792bb66520b4e3e9946853b450
superPuero <ar73mch1k@gmail.com>
2026-06-17 23:27:46 +0200

    make macos fix

--- jahoda/jahoda.mk
+++ jahoda/jahoda.mk
@@ -1,6 +1,8 @@
 CORE_SRCS = $(wildcard jahoda/core/*.c)
 BASE_SRCS = $(wildcard jahoda/base/*.c)
-GFX_SRCS = $(wildcard jahoda/gfx/*.c)
+# GFX_SRCS = $(wildcard jahoda/gfx/*.c)
 META_SRCS = $(wildcard jahoda/meta/*.c)
 
-SRCS += $(CORE_SRCS) $(BASE_SRCS) $(GFX_SRCS) $(META_SRCS)
+SRCS += $(CORE_SRCS) $(BASE_SRCS) $(META_SRCS)
+
+# SRCS += $(CORE_SRCS) $(BASE_SRCS) $(GFX_SRCS) $(META_SRCS)
